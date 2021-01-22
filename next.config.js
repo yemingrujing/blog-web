0e2741be3de66ef7792eb860be76862b992eb5c5
光伟 <yemingrujing@gmail.com>
2021-01-22 18:07:41 +0800

    首页

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withCss = require('@zeit/next-css');
 
 const configs = {
   // 输出目录
-  distDir: 'dest',
+  distDir: 'build',
   // 是否给每个路由生成Etag
   // Etag是用来做缓存验证的，如果路由执行的时候，新的Etag是相同的，那么就会复用当前内容，而无需重新渲染
   // 默认情况下，nextJS是会对每个路由生成Etag的。但是如果我们部署的时候，ngx已经做了Etag的配置，那么就可以关闭nextJS的Etag，节省性能
