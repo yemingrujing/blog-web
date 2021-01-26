c56429fd9580cfaefb8f97e042c4f2d8c0b44234
光伟 <yemingrujing@gmail.com>
2021-01-26 16:49:10 +0800

    博文detail

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,6 +37,11 @@ module.exports = {
       },
     },
   },
+  /**
+   * "off" 或 0 - 关闭规则
+   * "warn" 或 1 - 开启规则，使用警告级别的错误：warn (不会导致程序退出),
+   * "error" 或 2 - 开启规则，使用错误级别的错误：error (当被触发的时候，程序会退出)
+   */
   rules: {
     '@typescript-eslint/no-var-requires': 0,
     '@typescript-eslint/no-this-alias': ['off'],
