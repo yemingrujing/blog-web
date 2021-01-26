d01732662f4c2e07452eb865742f7e4a9d4b3887
光伟 <yemingrujing@gmail.com>
2021-01-26 16:47:28 +0800

    博文detail

--- pages/404.tsx
+++ pages/404.tsx
@@ -25,7 +25,7 @@ const Error = () => {
   return (
     <Container>
       <div className="out">
-        <img src="image-base-url/blog/common/404.gif" alt="404" />
+        <img src="/image-base-url/blog/common/404.gif" alt="404" />
         <div>
           <FrownOutlined />发生了一些意外... <b>{time}</b>秒后将 <span className="go">回到首页</span>
         </div>
