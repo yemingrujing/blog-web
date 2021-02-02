fc671617c1d0a7a3a8f3f3ca7475b5ea4c9d38e8
光伟 <yemingrujing@gmail.com>
2021-02-02 15:30:15 +0800

    优化

--- next.config.js
+++ next.config.js
@@ -6,12 +6,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 module.exports = withBundleAnalyzer({
   // 输出目录
   distDir: 'build',
-  assetPrefix: '',
-  // 是否给每个路由生成Etag
-  // Etag是用来做缓存验证的，如果路由执行的时候，新的Etag是相同的，那么就会复用当前内容，而无需重新渲染
-  // 默认情况下，nextJS是会对每个路由生成Etag的。但是如果我们部署的时候，ngx已经做了Etag的配置，那么就可以关闭nextJS的Etag，节省性能
-  generateEtags: false,
-  trailingSlash: true,
+  // 本地开发时对页面内容的缓存
+  onDemandEntries: {
+    // 内容在内存中缓存的时长(ms)
+    maxInactiveAge: 25 * 1000,
+    // 同时缓存的页面数
+    pagesBufferLength: 999,
+  },
   // 在pages目录下会被当做页面解析的后缀
   pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
   // 手动修改webpack配置
